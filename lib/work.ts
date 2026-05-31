681a641f8d990cd67ffed2516ab67d8ba208cbcb
Sanjaay Babu <babusanjaay@outlook.com>
2026-05-31 13:38:36 -0400

    updated work to include starling

--- lib/work.ts
+++ lib/work.ts
@@ -10,6 +10,18 @@ export type WorkItem = {
 
 export const workItems: WorkItem[] = [
   {
+    id: "multilateral-reform",
+    period: "05/2026-present",
+    role: "Undergraduate Policy Fellow",
+    org: "Starling Institute",
+    summary: "Research, staffing and tech development for an organization that seeks to enhance the state of global cooperation by building coalitions around key global issues.",
+    details: [
+      "Supporting the UN80 reform process by drafting analytical memos to support Member State partners",
+      "Following UN engagements relating to the selection of the next Secretary-General, setting the agenda and expectations for the next SG.",
+      "Integrating AI and creating bespoke software tools for the organization in order to save time and streamline processes for the Institute.",],
+    tags: ["Policy Research & Advocacy", "International Development", "AI integration and automation"],
+  }
+  {
     id: "development-economics",
     period: "02/2026–present",
     role: "Research Assistant",
